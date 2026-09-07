1654629daac1bdb57d69befd822de0f253cc4310
Sesham-Lavanya <lavanyasesham18@gmail.com>
2026-09-07 11:10:25 +0530

    Add files via upload

---
+++ if3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int units;
+	printf("enter units:");
+	scanf("%d",&units);
+	
+	if(units>300)
+	{
+		printf("High electricity usage");
+	}
+	return 0;
+}
---
+++ if4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("enter marks:");
+	scanf("%d",&marks);
+	
+	if(marks>=40&&marks<=100)
+	{
+		printf("Pass");
+	}
+	else
+	{
+		printf("fail");
+	}
+	return 0;
+}

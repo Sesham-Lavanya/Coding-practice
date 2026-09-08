3c3191ff4073a622b7683d787f239f9ab080cbf7
Sesham-Lavanya <lavanyasesham18@gmail.com>
2026-09-08 11:09:35 +0530

    Add files via upload

---
+++ relation 1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=30;
+	printf("10<5=%d",10<5);
+	printf("\n10>5=%d",10>5);
+	printf("\n10<=5=%d",10<=5);
+	printf("\n10>=5=%d",10>=5);
+	printf("\na=b=%d",a=b);
+	printf("\na==b=%d",a==b);
+	
+}
